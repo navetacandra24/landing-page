6033bebe9149d30f32dd94d777e40f15a2b7b9e5
navetacandra <70505125+navetacandra24@users.noreply.github.com>
2020-12-07 16:14:49 +0700

    Add files via upload

---
+++ 1/script.js
@@ -0,0 +1,4 @@
+window.addEventListener("load", function (e) {
+    document.querySelectorAll(".load-wraper")[0].style.background = "rgba(0,0,0,0)"
+    document.querySelectorAll(".load-wraper")[0].style.zIndex = "-999"
+})
